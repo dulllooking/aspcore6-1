646843c1d27bd1dfb2b9c085381a2d7d703e194d
Ulysses <dulllooking@hotmail.com>
2022-06-09 20:37:55 +0800

    上傳功能測試

---
+++ Controllers/CreateController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspcore6_1.Controllers
+{
+    public class CreateController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IFormFile FileUpload_FileName)   // 多檔上傳，請用 List<IFormFile>
+        {
+            try {
+                if (FileUpload_FileName.Length > 0)   // 檢查 <input type="file"> 是否輸入檔案？
+                {
+                    using (var ms = new MemoryStream()) {
+
+                        FileUpload_FileName.CopyTo(ms);
+                        if (ms.Length < 2097152) {
+                            var path = $@"Upload\{FileUpload_FileName.FileName}";
+                            using (var stream = new FileStream(path, FileMode.Create)) {
+                                FileUpload_FileName.CopyToAsync(stream);
+                            }
+                        }
+
+                    }
+
+                    ViewBag.Message = "上傳成功。";
+                }
+            }
+            catch {
+                ViewBag.Message = "上傳失敗。File upload failed!!";
+            }
+            return View();
+        }
+    }
+}
